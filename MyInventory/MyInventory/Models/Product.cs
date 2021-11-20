22b1e28abb725773f03cf70a0f0ed26374e90aa1
tahmidradit <tahmid.radit@outlook.com>
2021-11-20 19:02:49 +0600

    Added Product Class

---
+++ MyInventory/MyInventory/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyInventory.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        public double Price  { get; set; }
+        public string Image { get; set; }
+    }
+}
