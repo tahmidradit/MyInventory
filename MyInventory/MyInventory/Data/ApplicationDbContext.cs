9e855896aad0815b09d1ed43ac4b1c883e7ec806
tahmidradit <tahmid.radit@outlook.com>
2021-11-20 19:04:24 +0600

    Added Product DbSet in ApplicationDbContext

--- MyInventory/MyInventory/Data/ApplicationDbContext.cs
+++ MyInventory/MyInventory/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyInventory.Models;
 
 namespace MyInventory.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Product> Products { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
