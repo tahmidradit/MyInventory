6d76db574dee03d4dd5767a64e58ef8576204397
tahmidradit <tahmid.radit@outlook.com>
2023-11-23 00:48:10 +0600

    Removed docker support

--- MyInventory/MyInventory/Data/ApplicationDbContext.cs
+++ MyInventory/MyInventory/Data/ApplicationDbContext.cs
@@ -10,25 +10,6 @@ namespace MyInventory.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            try
-            {
-                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
-                if (databaseCreator != null)
-                {
-                    if (!databaseCreator.CanConnect())
-                    {
-                        databaseCreator.Create();
-                    }
-                    if (!databaseCreator.HasTables())
-                    {
-                        databaseCreator.CreateTables();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
