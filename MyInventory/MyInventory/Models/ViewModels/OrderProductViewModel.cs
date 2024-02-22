586eae3adee17a4e409c5ebea222c3477b067d75
tahmidradit <tahmid.radit@outlook.com>
2024-02-22 21:41:39 +0600

    Deleted all files from master

--- MyInventory/MyInventory/Models/ViewModels/OrderProductViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyInventory.Models.ViewModels
-{
-    public class OrderProductViewModel
-    {
-        public Product Product { get; set; }
-        public Order Order { get; set; }
-    }
-}
