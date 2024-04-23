2b904b5d15119d9cb28365964cae47c90c3ad241
tahmidradit <tahmid.radit@outlook.com>
2024-04-23 21:17:28 +0600

    Added EFCore

--- MyInventory/Program.cs
+++ MyInventory/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using MyInventory.Data;
 
 var builder = WebApplication.CreateBuilder(args);
