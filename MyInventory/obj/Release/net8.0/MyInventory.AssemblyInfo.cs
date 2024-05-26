78b7c813af492fe1d201828d1642695d3636ddcb
Tahmid Radit <49224955+tahmidradit@users.noreply.github.com>
2024-05-26 20:41:38 +0600

    ..

--- MyInventory/obj/Release/net8.0/MyInventory.AssemblyInfo.cs
+++ MyInventory/obj/Release/net8.0/MyInventory.AssemblyInfo.cs
@@ -11,12 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-MyInventory-2755A6CA-0436-44CF-9752-A315C3E579EF")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b904b5d15119d9cb28365964cae47c90c3ad241")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
